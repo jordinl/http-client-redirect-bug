263bcfe09d03c37b5d5f7725ea4c3390576fe952
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-07 11:18:55 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿var client = new HttpClient();
+
+string[] urls = ["https://google.com", "https://verizon.com"];
+
+foreach (var url in urls)
+{
+    var response = await client.GetAsync(url);
+    var status = response.StatusCode.GetHashCode();
+    var responseUrl = response.RequestMessage!.RequestUri!.ToString();
+    var locationHeader = response.Headers.Location;
+    Console.WriteLine($"{url}: Status {status}; Response URL: {responseUrl}; Location header: {locationHeader}");
+}
